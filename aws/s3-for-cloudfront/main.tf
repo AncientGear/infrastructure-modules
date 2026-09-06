8f81ccab6df74a61e2de8a9809b08bc823f756ce
saul <cpp.saul@gmail.com>
2026-09-06 14:49:42 -0600

    feat(s3)!: generate globally unique bucket names

--- aws/s3-for-cloudfront/main.tf
+++ aws/s3-for-cloudfront/main.tf
@@ -1,11 +1,18 @@
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+  numeric = true
+  lower   = true
+}
 resource "aws_s3_bucket" "this" {
-  bucket        = var.bucket_name
+  bucket        = "${var.bucket_prefix_name}-${random_string.random.result}"
   force_destroy = var.force_destroy
 
   tags = merge(
     var.tags,
     {
-      Name = var.bucket_name
+      Name = "${var.bucket_prefix_name}-${random_string.random.result}"
     }
   )
 }
--- aws/s3-for-cloudfront/variable.tf
+++ aws/s3-for-cloudfront/variable.tf
@@ -1,6 +1,25 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
+variable "bucket_prefix_name" {
+  description = "The prefix name of the S3 bucket"
   type        = string
+  validation {
+    condition     = length(var.bucket_prefix_name) > 0
+    error_message = "The bucket prefix name must not be empty."
+  }
+
+  validation {
+    condition     = length(var.bucket_prefix_name) <= 54
+    error_message = "The bucket prefix name must not exceed 54 characters."
+  }
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]+$", var.bucket_prefix_name))
+    error_message = "The bucket prefix name must only contain lowercase letters, numbers, and hyphens."
+  }
+
+  validation {
+    condition     = !can(regex("^-|-$", var.bucket_prefix_name))
+    error_message = "The bucket prefix name must not start or end with a hyphen."
+  }
 }
 
 variable "tags" {
