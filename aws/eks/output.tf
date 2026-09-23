077a5f47b7acfa018f1c6107b5b9c4d61750a4dc
saul <cpp.saul@gmail.com>
2026-09-23 12:00:30 -0600

    feat: expose eks oidc issuer outputs

--- aws/eks/output.tf
+++ aws/eks/output.tf
@@ -3,5 +3,16 @@ output "eks_name" {
 }
 
 output "openid_provider_arn" {
-  value = aws_iam_openid_connect_provider.this[0].arn
+  description = "ARN of the IAM OIDC provider created for IRSA, or null when IRSA is disabled."
+  value       = try(aws_iam_openid_connect_provider.this[0].arn, null)
+}
+
+output "oidc_provider_arn" {
+  description = "Alias for openid_provider_arn, matching IAM role module input naming. Null when IRSA is disabled."
+  value       = try(aws_iam_openid_connect_provider.this[0].arn, null)
+}
+
+output "oidc_provider_url" {
+  description = "OIDC issuer URL for the EKS cluster."
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
